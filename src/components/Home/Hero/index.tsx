7719dd80a318f90624c8ffc82fb31876013b944e
Faisal Amir <urmauur@gmail.com>
2024-03-26 23:45:09 +0700

    chore: added banner link with new target

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -8,13 +8,13 @@ const Hero = () => {
   return (
     <div className="nextra-wrap-container">
       <div className="mt-10 text-center">
-        <Link href="https://github.com/janhq/jan/releases">
+        <Link href="https://github.com/janhq/jan/releases" target="_blank">
           <div className="inline-flex my-2 py-2 pl-2 pr-4 rounded-lg mb-10 bg-indigo-500 text-white">
             <div className="flex items-center rounded bg-white px-2">
               <span className="font-bold uppercase text-blue-600">new</span>
             </div>
             &nbsp;✨&nbsp; <b>{lastVersion}</b>&nbsp;is now live on
-            GitHub.&nbsp; Check it out!
+            GitHub.&nbsp;Check it out!
           </div>
         </Link>
         <h1 className="text-6xl lg:text-[80px] !font-normal leading-tight lg:leading-tight mt-2 font-serif">
