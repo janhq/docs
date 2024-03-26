316f71e16351d191bca2cbd15f4904356d4c5f3b
Faisal Amir <urmauur@gmail.com>
2024-03-26 19:58:41 +0700

    feat: initial setup nextra

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  darkMode: ['class'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './theme.config.tsx',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    fontFamily: {
+      sans: [
+        'Inter',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Oxygen-Sans',
+        'Ubuntu,Cantarell',
+        'Helvetica',
+        'sans-serif',
+      ],
+      serif: ['PPEditorialNew'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+export default config
