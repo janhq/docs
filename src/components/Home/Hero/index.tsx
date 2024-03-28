7dd2adab9a480ffb5fe614fd7e27bd8f5369fea9
Faisal Amir <urmauur@gmail.com>
2024-03-28 10:32:30 +0700

    chore: fix responsive menu navbar

--- src/components/Home/Hero/index.tsx
+++ src/components/Home/Hero/index.tsx
@@ -9,7 +9,11 @@ const Hero = () => {
   return (
     <div className="nextra-wrap-container">
       <div className="mt-10 text-center">
-        <Link href="https://github.com/janhq/jan/releases" target="_blank">
+        <Link
+          href="https://github.com/janhq/jan/releases"
+          target="_blank"
+          className="hidden lg:block"
+        >
           <div className="inline-flex my-2 py-2 pl-2 pr-4 rounded-lg mb-10 bg-indigo-500 text-white">
             <div className="flex items-center rounded bg-white px-2">
               <span className="font-bold uppercase text-blue-600">new</span>
