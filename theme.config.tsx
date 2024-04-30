8d3e036e13e924f66604db0002642a8892a8d2b1
irfanpena <irfan@penateam.com>
2024-04-30 14:57:18 +0700

    WIP Cortex Branch

--- theme.config.tsx
+++ theme.config.tsx
@@ -55,8 +55,9 @@ const config: DocsThemeConfig = {
         return (
           <div className="-mx-2 hidden md:block">
             {[
-              { title: 'Jan', path: '/docs', Icon: LibraryBig },
-              { title: 'Integrations', path: '/integrations', Icon: Blocks },
+              { title: "Jan", path: "/docs", Icon: LibraryBig },
+              { title: "Cortex", path: "/cortex", Icon: BrainCircuit },
+              { title: "Integrations", path: "/integrations", Icon: Blocks }
             ].map((item) =>
               asPath.startsWith(item.path) ? (
                 <div
